6d9a57c2fbd5e2e61e43fa8e3a395328a8d0836d
Michael Mangino <mmangino@elevatedrails.com>
2008-07-03 08:13:52 -0400

    Add timeout

--- lib/tunnlr/connector.rb
+++ lib/tunnlr/connector.rb
@@ -11,18 +11,26 @@ module Tunnlr
     
     def connect!
       @should_disconnect=false
-      Net::SSH.start(host,username,:password=>password) do |ssh|
-        puts "Connecting to #{username}@#{host} and sending #{remote_port}->#{local_port}"
-        ssh.forward.remote_to(local_port,'127.0.0.1',remote_port,'0.0.0.0')
-        puts "You can view your tunneled connection at http://web1.tunnlr.com:#{remote_port}/"
-        while true
-          begin
-            ssh.loop {!@should_disconnect}
-          rescue Interrupt=>e
-            raise
-          rescue Exception=>e
-            puts "Swallowing: #{e.class.to_s}"
+      while true do
+        begin
+          Net::SSH.start(host,username,:password=>password) do |ssh|
+            puts "Connecting to #{username}@#{host} and sending #{remote_port}->#{local_port}"
+            ssh.forward.remote_to(local_port,'127.0.0.1',remote_port,'0.0.0.0')
+            puts "You can view your tunneled connection at http://web1.tunnlr.com:#{remote_port}/"
+            while true
+              begin
+                ssh.loop {!@should_disconnect}
+              rescue Errno::ECONNRESET=>e
+                raise
+              rescue Interrupt=>e
+                raise
+              rescue Exception=>e
+                puts "Swallowing: #{e.class.to_s}"
+              end
+            end
           end
+        rescue Errno::ECONNRESET=>e
+          puts "Your connection was reset, trying to restart ..."
         end
       end
     end

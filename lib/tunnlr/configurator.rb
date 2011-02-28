a027149abee0308d3090dca9161e7006067fb9c9
Mike Mangino <mmangino@elevatedrails.com>
2011-02-28 15:28:02 -0500

    Update to include subdomain

--- lib/tunnlr/configurator.rb
+++ lib/tunnlr/configurator.rb
@@ -2,14 +2,14 @@ require 'highline'
 require 'net/ssh'
 module Tunnlr
   class Configurator
-    attr_accessor :not_configured,:email,:password
+    attr_accessor :not_configured,:email,:password,:subdomain
     def initialize
       @ui = HighLine.new
       @not_configured=true
     end
     
     def fetch(username,password)
-      url=URI.parse('http://tunnlr.com/configuration.yml')
+      url=URI.parse("http://#{subdomain}.tunnlr.com/configuration.yml")
       req = Net::HTTP::Get.new(url.path)
       req.basic_auth(username,password)
       res = Net::HTTP.start(url.host, url.port) {|http|
@@ -30,6 +30,7 @@ module Tunnlr
     
     def get_credentials
       puts "Enter the email address and password you used to sign up for Tunnlr."
+      self.subdomain = @ui.ask("Subdomain (i.e. elevatedrails for elevatedrails.tunnlr.com): ") 
       self.email = @ui.ask("Email: ") 
       self.password = @ui.ask("Password: ") { |q| q.echo = false }
       fetch(email,password)
@@ -39,7 +40,7 @@ module Tunnlr
       while not_configured
         begin
           get_credentials
-          fetch(email,password)
+          fetch(subdomain,email,password)
           add_password
           write_config(path)
           puts "Created configuration in #{path}"
